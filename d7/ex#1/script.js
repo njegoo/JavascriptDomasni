f2fa4af10737c9b6cf9cc7cdef50888d075a10f1
njegoo <112905929+njegoo@users.noreply.github.com>
2023-05-23 18:01:02 +0200

    domasno od cas 7

---
+++ d7/ex#1/script.js
@@ -0,0 +1,9 @@
+let logo = document.querySelector(".lnXdpd");
+logo.setAttribute(
+  "src",
+  "https://s.yimg.com/rz/p/yahoo_homepage_en-US_s_f_p_bestfit_homepage.png"
+);
+logo.setAttribute(
+  "srcset",
+  "https://s.yimg.com/rz/p/yahoo_homepage_en-US_s_f_p_bestfit_homepage.png"
+);
---
+++ d7/ex#2/script.js
@@ -0,0 +1,5 @@
+// console.log({ document });
+// let body = document.getElementsByName("body");
+// console.log(body);
+// body.style.fontFamily = "Arial, sans-serif";
+// body.setAttribute("font-family", "Arial, sans-serif");
